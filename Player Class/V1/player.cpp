ef34146fdcfc559d95999fa2d6a3ef6435b7bf2a
JunweiYan <67297630+JunweiYan@users.noreply.github.com>
2021-04-15 00:23:10 -0700

    Update player.cpp

--- Player Class/V1/player.cpp
+++ Player Class/V1/player.cpp
@@ -1,72 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   player.cpp
- * Author: daysk
- *
- * Created on 2021年4月5日, 下午7:34
- */
-
 #include <iostream>
 #include "player.h"
 
-
-int itemclass::getNum() {
-	return this->ItemNum;
-}
-
-string itemclass::getName() {
-	return this->ItemName;
-}
-
-void itemclass::setNum(int num) {
-	this->ItemNum = num;
-}
-
-void itemclass::showItem() {
-	cout << "Item name: " << this->ItemName << endl;
-	cout << "Quantity: " << this->ItemNum << endl;
-}
+using namespace std;
 
 
-void playerclass::ShowItemInfo() {
-	for (int i = 0; i < this->bag.size(); i++) {
-		this->bag[i].showItem();
-		cout << endl;
+int main() 
+{
+	object drug, ball, revival;
+	vector<object> bag;
+
+	//Ten bottles of potin were given at the beginning.
+	drug.num = 10;
+	drug.name = "potion";
+
+	//Transfer the data of the quantity and name of the liquid medicine into the vector
+	bag.push_back(drug);
+
+	//ten poke ball were given at the beginning.
+	ball.num = 10;
+	ball.name = "pokeball";
+	bag.push_back(ball);
+
+	//five revives were given at the beginning
+	revival.num = 5;
+	revival.name = "revival";
+	bag.push_back(revival);
+
+	pokemon po;
+	//allow user to change the nickname for pokemon
+	cout << "Please enter your pokemon's nickname: ";
+	cin >> po.nickname;
+
+	vector<pokemon> poke(6);
+	poke.push_back(po);
+	return 0;
 	}
-}
-//
-//void playerclass::getitem(itemclass) {
-//	this->bag.push_back(drug);
-//	this->bag.push_back(pokeball);
-//	this->bag.push_back(revival);
-//}
-
-bool playerclass::useItem(string itemName, int num) {
-	for (int i = 0; i < this->bag.size(); i++) {
-		if (this->bag[i].getName() == itemName) {
-			int presentCnt = this->bag[i].getNum();
-			if (presentCnt < num)
-			{
-				cout << "Insufficient quantity, failed to use." << endl;
-				return false;
-			}
-			else
-			{
-				int newNum = this->bag[i].getNum() - num;
-				this->bag[i].setNum(newNum);
-				cout << "successful use." << endl;
-				return true;
-			}
-		}
-		else
-			continue;
-	}
-}
-
-
 
